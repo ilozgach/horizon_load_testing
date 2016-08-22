d1894f38c3e56ee3d341b2b537335356af634ce2
pashkash <pashka.shvetsov@gmail.com>
2016-08-22 18:18:10 +0300

    add pulling result for testrail api

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
+import datetime
 import json
+import os
 import threading
+import time
 import unittest
 import urlparse
 
 import ddt as ddt
-import time
+import helpers.testrail_api as testrail_api
+import numpy
 
-import os
 from pyvirtualdisplay import Display
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 from custom_openstack_client import CustomOpenstackClient
 from custom_report import CustomReport
@@ -26,16 +29,20 @@ display.start()
 class HorizonLoadTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
+        print "Call setUpClass"
         scr_dir = os.path.dirname(os.path.realpath(__file__))
         with open(os.path.join(scr_dir, "conf.json"), "r") as cf:
             cls.conf = json.load(cf)
         cls.report = CustomReport()
         cls.client = CustomOpenstackClient(cls.conf)
+        cls.test_run = testrail_api.TestRailRun(cls.conf)
 
     @classmethod
     def tearDownClass(cls):
+        print "Call tearDownClass"
         cls.report.write_results()
         cls.client.cleanup()
+
 
     def login_driver(self, driver):
         login_url = urlparse.urljoin(self.conf["horizon_base_url"], "auth/login")
@@ -49,8 +56,10 @@ class HorizonLoadTest(unittest.TestCase):
                 input_login.send_keys("admin")
                 input_password.send_keys("admin")
                 driver.find_element_by_id("loginBtn").click()
+                print "Login OK"
         except NoSuchElementException:
-            pass
+            print "Login NOT OK!"
+            raise Exception("Login NOT OK!")
 
     def set_number_of_items_per_page(self, driver, nof_items):
         if nof_items > 1000:
@@ -63,6 +72,7 @@ class HorizonLoadTest(unittest.TestCase):
         input_pagesize.send_keys(str(nof_items))
         btn_save = driver.find_element_by_class_name("btn")
         btn_save.click()
+        print "Number items on pages set to {}".format(nof_items)
 
     # def set_volumes_quota(self, nof_volumes):
     #     quotas_url = urlparse.urljoin(self.conf["horizon_base_url"], "admin/defaults")
@@ -86,8 +96,10 @@ class HorizonLoadTest(unittest.TestCase):
                 self.assertTrue("Displaying {} items".format(nof_instances) in count_span.text)
                 results.append(te - ts)
             except NoSuchElementException:
+                print "NoSuchElementException"
                 nof_fails += 1
             except TimeoutException:
+                print "TimeoutException"
                 nof_fails += 1
             if nof_fails > int(times / 100 * 5):
                 print "Number of fails exceeded, nof_fails = {}, iteration = {}".format(nof_fails, i)
@@ -98,136 +110,164 @@ class HorizonLoadTest(unittest.TestCase):
          "instance_generator": "generate_images",
          "nof_instances": 100,
          "times": 100,
-         "concurrency": 1},
+         "concurrency": 1,
+         "test_case_id": 1673818},
         {"page": "admin/images",
          "instance_generator": "generate_images",
          "nof_instances": 500,
          "times": 100,
-         "concurrency": 1},
+         "concurrency": 1,
+         "test_case_id": 1673819},
 
         {"page": "admin/volumes",
          "instance_generator": "generate_volumes",
          "nof_instances": 100,
          "times": 100,
-         "concurrency": 1},
+         "concurrency": 1,
+         "test_case_id": 1673820},
         {"page": "admin/volumes",
          "instance_generator": "generate_volumes",
          "nof_instances": 500,
          "times": 100,
-         "concurrency": 1},
+         "concurrency": 1,
+         "test_case_id": 1673821},
 
         {"page": "project/volumes",
          "instance_generator": "generate_volumes",
          "nof_instances": 100,
          "times": 100,
-         "concurrency": 1},
+         "concurrency": 1,
+         "test_case_id": 1681252},
         {"page": "project/volumes",
          "instance_generator": "generate_volumes",
          "nof_instances": 500,
          "times": 100,
-         "concurrency": 1},
+         "concurrency": 1,
+         "test_case_id": 1681253},
 
         {"page": "admin/volumes/?tab=volumes_group_tabs__snapshots_tab",
          "instance_generator": "generate_volume_snapshots",
          "nof_instances": 100,
          "times": 100,
-         "concurrency": 1},
+         "concurrency": 1,
+         "test_case_id": 1673822},
         {"page": "admin/volumes/?tab=volumes_group_tabs__snapshots_tab",
          "instance_generator": "generate_volume_snapshots",
          "nof_instances": 500,
          "times": 100,
-         "concurrency": 1},
+         "concurrency": 1,
+         "test_case_id": 1673823},
 
         {"page": "identity/users",
          "instance_generator": "generate_users",
          "nof_instances": 100,
          "times": 100,
-         "concurrency": 1},
+         "concurrency": 1,
+         "test_case_id": 1673824},
         {"page": "identity/users",
          "instance_generator": "generate_users",
          "nof_instances": 500,
          "times": 100,
-         "concurrency": 1},
+         "concurrency": 1,
+         "test_case_id": 1673825},
 
         {"page": "identity",
          "instance_generator": "generate_projects",
          "nof_instances": 100,
          "times": 100,
-         "concurrency": 1},
+         "concurrency": 1,
+         "test_case_id": 1673826},
         {"page": "identity",
          "instance_generator": "generate_projects",
          "nof_instances": 500,
          "times": 100,
-         "concurrency": 1},
+         "concurrency": 1,
+         "test_case_id": 1673827},
 
         {"page": "admin/instances",
          "instance_generator": "generate_instances",
          "nof_instances": 30,
          "times": 100,
-         "concurrency": 1},
+         "concurrency": 1,
+         "test_case_id": 1673828},
         {"page": "admin/instances",
          "instance_generator": "generate_instances",
          "nof_instances": 70,
          "times": 100,
-         "concurrency": 1},
+         "concurrency": 1,
+         "test_case_id": 1673829},
 
         {"page": "project/instances",
          "instance_generator": "generate_instances",
          "nof_instances": 30,
          "times": 100,
-         "concurrency": 1},
+         "concurrency": 1,
+         "test_case_id": 1681254},
         {"page": "project/instances",
          "instance_generator": "generate_instances",
          "nof_instances": 70,
          "times": 100,
-         "concurrency": 1},
+         "concurrency": 1,
+         "test_case_id": 1681255},
 
         {"page": "admin/networks",
          "instance_generator": "generate_networks",
          "nof_instances": 100,
          "times": 100,
-         "concurrency": 1},
+         "concurrency": 1,
+         "test_case_id": 1673830},
         {"page": "admin/networks",
          "instance_generator": "generate_networks",
          "nof_instances": 500,
          "times": 100,
-         "concurrency": 1},
+         "concurrency": 1,
+         "test_case_id": 1673831},
 
         {"page": "admin/routers",
          "instance_generator": "generate_routers",
          "nof_instances": 100,
          "times": 100,
-         "concurrency": 1},
+         "concurrency": 1,
+         "test_case_id": 1673832},
         {"page": "admin/routers",
          "instance_generator": "generate_routers",
          "nof_instances": 500,
          "times": 100,
-         "concurrency": 1},
+         "concurrency": 1,
+         "test_case_id": 1673833},
 
         {"page": "admin/flavors",
          "instance_generator": "generate_flavors",
          "nof_instances": 100,
          "times": 100,
-         "concurrency": 1},
+         "concurrency": 1,
+         "test_case_id": 1673834},
         {"page": "admin/flavors",
          "instance_generator": "generate_flavors",
          "nof_instances": 500,
          "times": 100,
-         "concurrency": 1}
+         "concurrency": 1,
+         "test_case_id": 1673835}
     )
     @ddt.unpack
-    def test_page(self, page, instance_generator, nof_instances, times, concurrency):
+    def test_page(self, page, instance_generator, nof_instances, times, concurrency, test_case_id=None):
         print "Testing {} page".format(page)
         page_url = urlparse.urljoin(self.conf["horizon_base_url"], page)
         getattr(self.client, instance_generator)(nof_instances)
 
         if concurrency == 1:
-            driver = webdriver.Chrome()
+            start = datetime.datetime.now()
+
+            driver = webdriver.Firefox()
             self.login_driver(driver)
             self.set_number_of_items_per_page(driver, nof_instances)
 
             results = []
             self.worker(driver, page_url, nof_instances, times, results)
+
+            percentile = numpy.percentile(results, 90)
+            time_elapsed_sec = (datetime.datetime.now() - start).seconds
+            self.test_run.add_result(test_case_id, percentile, time_elapsed_sec)
 
             for result in results:
                 self.report.add_result(page, nof_instances, result)
